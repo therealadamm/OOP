5a10524b3c2a17909c0034285c6270e93681face
therealdamm <adamfahmi008@gmail.com>
2024-05-27 18:34:44 +0800

    masuk oi

--- src/CourseEnrollment.java
+++ src/CourseEnrollment.java
@@ -2,13 +2,49 @@ public class CourseEnrollment {
     
 }
 
+class Person{
 
+}
+class Professor{
+
+}
 class Student{
     
 }
 
 class Department{
-    
+
 }
 
+class CourseCatalog{
+
+}
+
+class Administrator{
+
+}
+
+class University{
+
+}
+
+class Course{
+
+}
+
+class Schedule{
+
+}
+
+class Grade{
+
+}
+
+class Transcript{
+
+}
+
+class Enrollment{
+    
+}
 
